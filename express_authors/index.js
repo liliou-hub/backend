29dba191bed08d81c085b04a4194881172ff9df5
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-10-28 16:38:07 +0100

    jour 1

---
+++ express_authors/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+const authorsOrder = ['Lawrence Nowell, UK','William Shakespeare, UK','Charles Dickens, US','Oscar Wilde, UK'];
+// console.log(authors);
+
+app.get('/', (req, res) => {
+    res.send('Authors API' );
+  });
+
+
+app.get('/author/:authorsOrder', (req, res) => {
+  res.send(`Authors API :${authorsOrder[req.params.authorsOrder]}`);
+});
+// app.get('/authors/:1', (req, res) => {
+//     res.send('Lawrence Nowell, UK' );
+// });
+// app.get('/authors/:2/', (req, res) => {
+//     res.send('William Shakespeare, UK ');
+// });
+// app.get('/authors/:3/', (req, res) => {
+//     res.send('Charles Dickens, US');
+// });
+// app.get('/authors/:4/', (req, res) => {
+//     res.send('Oscar Wilde, UK');
+// });
+
+
+app.listen(port, () => {
+  console.log('Server started on port: ' + port);
+});
