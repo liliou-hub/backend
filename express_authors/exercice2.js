67338c1532afba7c359e7e40c7cccd28dda2c4a8
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-10-30 16:56:37 +0100

    exercice 4!

--- express_authors/exercice2.js
+++ express_authors/exercice2.js
@@ -10,30 +10,33 @@ const all=[
     {
         id: 0,
         name: "Lawrence Nowell",
-        nationality: "UK"
+        nationality: "UK",
+        books:'Beowulf'
     },
 
     {
         id: 1,
         name: "William Shakespeare",
-        nationality: "UK"
+        nationality: "UK",
+        books:'Hamlet, Othello, Romeo and Juliet, MacBeth'
     },
 
     {
         id: 2,
         name: "Charles Dickens",
-        nationality: "Us"
+        nationality: "Us",
+        books:  'Oliver Twist, A Christmas Carol'
     },
 
     {
         id: 3,
         name: "Oscar Wilde",
-        nationality: "UK"
+        nationality: "UK",
+        books:'The Picture of Dorian Gray, The Importance of Being Earnest'
+
     },
     
 ]
-
-
 
 app.get('/', (req, res) => {
     res.send('Authors API');
@@ -47,7 +50,6 @@ app.get('/authors/:ID', function (req, res) {
 app.get('/authors/:authorsOrder', (req, res) => {
     res.send(` authors:${authorsOrder[req.params.authorsOrder]}`);
 });
-
 
 
 app.get('/authors/:books', (req, res) => {
