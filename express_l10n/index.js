3cb82df614bdc65e8b34eda82fe4b7b3e90a5264
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-11-09 17:00:52 +0100

    school

---
+++ express_l10n/index.js
@@ -0,0 +1,32 @@
+const app = express();
+const exphbrs = require('express-handlebars')
+
+app.engine('handlebars', exphbrs({
+    defaultLayout: false,
+    layoutsDir: __dirname + "views/"
+}));
+
+app.set('view engine', 'handlebars');
+
+
+app.get('/:lang?', (req, res) => {
+    res.render('home', {
+       lang:fr
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+});
