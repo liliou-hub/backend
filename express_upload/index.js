f3e4fc357849058efa39b19dbb8d6e762ebd767c
liliou-hub <68587909+liliou-hub@users.noreply.github.com>
2020-11-13 21:54:47 +0100

    upload etape5

--- express_upload/index.js
+++ express_upload/index.js
@@ -1,118 +1,86 @@
-const express = require('express');
-const exphbrs = require('express-handlebars')
+const express = require("express");
+const exphbrs = require("express-handlebars");
+const multer = require("multer");
 const app = express();
-const port = 3000
-const multer = require('multer');
-const upload = multer({ dest: 'public/uploads/' });
-
-
-
-const mongoose = require('mongoose')
-
-app.engine('handlebars', exphbrs({}));
-app.set('view engine', 'handlebars');
-
-
-
-mongoose.connect('mongodb://localhost:27017/upload', {
+const port = 3000;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/upload", {
+    useUnifiedTopology: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch(err => console.log(err))
-
-const userSchema = new mongoose.Schema({
-    username: {
-        type: [String],
-        index: true
-        // required: true,
-    },
-    firstname: {
-        type: String,
-        // required: true,
-    },
-    surname: {
-        type: String,
-        // required: true,
-    },
-    profilpicture: {
-        type: String,
-        // required: true,
-    },
 });
 
-const userModel = mongoose.model('Users', userSchema);
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        // console.log('file multer diskstorage', file);
+        //   cb(null, file.originalname)
+        let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+        cb(null, Date.now() + ext)
+    }
+})
+
+var upload = multer({ storage: storage })
 
 
 
+app.set("view engine", "handlebars");
 
+app.engine("handlebars", exphbrs());
 
-
-app.get('/', (req, res, next) => {
-    res.render('home', {
-        title: 'HELLO USERS',
+app.get("/", (req, res) => {
+    res.render("home", {
+        title: "New user",
     });
 });
 
+const uploadSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        index: true,
+    },
+    firstName: String,
+    surname: String,
+    profilePicture: String,
+});
 
+const User = mongoose.model("User", uploadSchema);
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.post("/upload", upload.single("avatar"), async (req, res, next) => {
+    let uservar = req.body.username;
 
+    const newUser = await User.create({
+        username: uservar,
+        profilePicture: req.file.filename,
+    });
 
-app.post('/upload', upload.single('img'), (req, res, next) => {
+    console.log('newuser', newUser)
 
-    let usernamevar = req.body.username
-    let img2 = req.file
+    res.render("uploadadd", {
+        username: uservar,
+        id: newUser._id
+    });
 
-    res.render('uploadadd', {
-        username: usernamevar,
-        img: img2
-    })
-
-    userModel.create({
-        username: usernamevar,
-        profilpicture: img2.path
-
-    }).then(data => console.log(data))
-        .catch(err => console.log(err))
-
-})
-
-let iduser = "5faea178a861053e50047ae6"
-
-
-app.get('/users/:id/',  (req, res) => {
-    let result = UserModel.findById(`${iduser}`, function (err, result) {
-        console.log('lelele', result.username)
-        res.render('userpage',
-            { username: result.username })
-    })
 });
 
+app.get("/users/:id/", (req, res) => {
+    let result = User.findById(req.params.id, function (err, result) {
+        console.log('lelele', result)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        res.render("userspage", {
+            username: result.username,
+            profilePicture: result.profilePicture,
+        });
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
-
